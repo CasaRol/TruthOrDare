fcf4340b9021c62917fd722f4449439a543278c4
CasaRol <32219634+CasaRol@users.noreply.github.com>
2020-11-23 15:52:51 +0100

    Added files to read from at system start
    Dare needs to be written.

--- TruthOrDare/src/truthordare/TruthOrDare.java
+++ TruthOrDare/src/truthordare/TruthOrDare.java
@@ -11,9 +11,6 @@ package truthordare;
  */
 public class TruthOrDare {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
